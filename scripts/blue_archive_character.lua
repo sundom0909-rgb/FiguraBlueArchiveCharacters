60e98edc5daf8e8ce8a3b291279428e37c1de7b0
Gakuto1112 <gakuto131112@gmail.com>
2024-12-20 01:39:18 +0900

    Exスキル1のカメラワークを作成

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -451,13 +451,13 @@ BlueArchiveCharacter = {
 
                 camera = {
                     start = {
-                        rot = vectors.vec3(0, 180, 0);
-                        pos = vectors.vec3(0, 28, -64);
+                        rot = vectors.vec3(0, 155, 0);
+                        pos = vectors.vec3(0, 43, -28);
                     };
 
                     fin = {
-                        rot = vectors.vec3(0, 180, 0);
-                        pos = vectors.vec3(0, 28, -64);
+                        rot = vectors.vec3(-5, 280, 0);
+                        pos = vectors.vec3(-8, 60.4, -388);
                     };
                 };
             };
@@ -615,7 +615,7 @@ BlueArchiveCharacter = {
         --Player APIにアクセスする場合は、ENTITY_INIT後に実行されるようにする必要がある。
 
         for _, modelPart in ipairs({models.models.ex_skill_1.Tank.TankBody.PSLogo1, models.models.ex_skill_1.Tank.TankBody.Turret.PSLogo2, models.models.ex_skill_1.Tank.TankBody.Turret.PSLogo3}) do
-            modelPart:newText("toramaru_logo_text"):setPos(0, 2.25, 0):setScale(0.2):setAlignment("CENTER"):setOutline(true)
+            modelPart:newText("toramaru_logo_text"):setText("§e万魔殿"):setPos(0, 2.25, 0):setScale(0.2):setAlignment("CENTER"):setOutline(true):setOutlineColor(0.404, 0.306, 0.051)
         end
         models.models.ex_skill_1.Tank.TankBody.Turret.Cannon.HangingSign:setPrimaryTexture("RESOURCE", "minecraft:textures/entity/signs/hanging/oak.png")
         models.models.ex_skill_1.Tank.TankBody.Turret.Cannon.HangingSign:newText("toramaru_sign_text_1"):setText("§0§l巡回中"):setPos(-1, -9, 0.5):setRot(0, 90, 0):setScale(0.5):setAlignment("CENTER")
@@ -638,15 +638,6 @@ BlueArchiveCharacter = {
                         models.models.ex_skill_1.Tank:setVisible(true)
                         models.models.main.Avatar:setPos(-13, 16, 4)
                         models.models.ex_skill_1.Tank:setOffsetPivot(0, 0, 8)
-                        models.models.ex_skill_1.Tank:setColor(1, 1, 1)
-                        for _, modelPart in ipairs({models.models.ex_skill_1.Tank.TankBody.PSLogo1, models.models.ex_skill_1.Tank.TankBody.Turret.PSLogo2, models.models.ex_skill_1.Tank.TankBody.Turret.PSLogo3}) do
-                            modelPart:getTask("toramaru_logo_text"):setText("§e万魔殿"):setOutlineColor(0.404, 0.306, 0.051)
-                        end
-                        for i = 0, 1 do
-                            for j = 0, 9 do
-                                models.models.ex_skill_1.Tank.TankBody.BaseBase1:getTask("toramaru_log_"..(i * 10 + j)):setLight()
-                            end
-                        end
                         self.parent.cameraManager:setThirdPersonCameraDistance(8)
                         self.parent.arms:setArmState(0, 0)
                         animations["models.main"]["tank_start"]:play()
@@ -787,6 +778,15 @@ BlueArchiveCharacter = {
                         for _, modelPart in ipairs({models.models.ex_skill_1.Tank, models.models.ex_skill_1.Tank.TankBody.Turret, models.models.ex_skill_1.Tank.TankBody.Turret.Cannon}) do
                             modelPart:setPos()
                             modelPart:setRot()
+                        end
+                        models.models.ex_skill_1.Tank:setColor(1, 1, 1)
+                        for _, modelPart in ipairs({models.models.ex_skill_1.Tank.TankBody.PSLogo1, models.models.ex_skill_1.Tank.TankBody.Turret.PSLogo2, models.models.ex_skill_1.Tank.TankBody.Turret.PSLogo3}) do
+                            modelPart:getTask("toramaru_logo_text"):setText("§e万魔殿"):setOutlineColor(0.404, 0.306, 0.051)
+                        end
+                        for i = 0, 1 do
+                            for j = 0, 9 do
+                                models.models.ex_skill_1.Tank.TankBody.BaseBase1:getTask("toramaru_log_"..(i * 10 + j)):setLight()
+                            end
                         end
                         models.models.main.Avatar:setPos()
                         self.parent.cameraManager:setThirdPersonCameraDistance(4)
