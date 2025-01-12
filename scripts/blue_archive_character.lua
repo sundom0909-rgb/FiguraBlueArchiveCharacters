41145ff9d26fff08adb4614c08e5e290e7987f2b
Gakuto1112 <gakuto131112@gmail.com>
2025-01-12 15:15:05 +0900

    固有武器のモデルを作成

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -314,31 +314,31 @@ BlueArchiveCharacter = {
         }
 
         instance.gun = {
-            scale = 1.2;
+            scale = 1.5;
 
             gunPosition = {
                 hold = {
                     type = "NORMAL";
+
+                    firstPersonPos = {
+                        right = vectors.vec3(-0.5, 1.5, -2);
+                        left = vectors.vec3(0.5, 1.5, -2);
+                    };
+
+                    thirdPersonPos = {
+                        right = vectors.vec3(-1.75, 0, -4);
+                        left = vectors.vec3(1.75, 0, -4);
+                    };
                 };
 
                 put = {
-                    type = "BODY";
-
-                    pos = {
-                        right = vectors.vec3(4.5, -3, 4);
-                        left = vectors.vec3(-4.5, -3, 4);
-                    };
-
-                    rot = {
-                        right = vectors.vec3(-90, 0, 0);
-                        left = vectors.vec3(-90, 0, 0);
-                    };
+                    type = "HIDDEN";
                 };
             };
 
             sound = {
-                name = "minecraft:entity.iron_golem.hurt";
-                pitch = 2;
+                name = "minecraft:entity.firework_rocket.blast";
+                pitch = 1;
             };
         }
 
