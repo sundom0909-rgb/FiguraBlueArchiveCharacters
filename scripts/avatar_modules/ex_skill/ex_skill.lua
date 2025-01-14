bef4b35c029bc90581e485ef232476a17441defb
Gakuto1112 <gakuto131112@gmail.com>
2025-01-14 19:51:16 +0900

    デバッグモードの解除

--- scripts/avatar_modules/ex_skill/ex_skill.lua
+++ scripts/avatar_modules/ex_skill/ex_skill.lua
@@ -31,7 +31,7 @@ ExSkill = {
         ---@type ExSkill
         local instance = Avatar.instantiate(ExSkill, AvatarModule, parent)
 
-        instance.AUTO_PLAY = "SUB"
+        instance.AUTO_PLAY = "NONE"
 
         instance.frameParticleAmount = instance.parent.config:loadConfig("PRIVATE", "exSkillFrameParticleAmount", 1)
         instance.exSkillIndex = 1
