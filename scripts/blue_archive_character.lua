0b4b95714d03af93f17c8954278afdfc12551210
Gakuto1112 <gakuto131112@gmail.com>
2025-03-02 15:15:25 +0900

    Exスキル4のゾンビの鉄防具のテクスチャの参照を修正 関連issue: #112

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -1049,11 +1049,12 @@ BlueArchiveCharacter = {
                     onPreAnimation = function (self)
                         if not self.exSkill[4].init then
                             models.models.ex_skill_4.Zombie:setPrimaryTexture("RESOURCE", "minecraft:textures/entity/zombie/zombie.png")
+                            local gameVersion = client:getVersion()
                             for _, modelPart in ipairs({models.models.ex_skill_4.Zombie.ZHead.ZHelmet, models.models.ex_skill_4.Zombie.ZUpperBody.ZBody.ZChestPlateB, models.models.ex_skill_4.Zombie.ZUpperBody.ZArms.ZRightArm.ZChestPlateRA, models.models.ex_skill_4.Zombie.ZUpperBody.ZArms.ZLeftArm.ZChestPlateLA, models.models.ex_skill_4.Zombie.ZLowerBody.ZLegs.ZRightLeg.ZBootsRL, models.models.ex_skill_4.Zombie.ZLowerBody.ZLegs.ZLeftLeg.ZBootsLL}) do
-                                modelPart:setPrimaryTexture("RESOURCE", "minecraft:textures/models/armor/iron_layer_1.png")
+                                modelPart:setPrimaryTexture("RESOURCE", gameVersion >="1.21.2" and "minecraft:textures/entity/equipment/humanoid/iron.png" or "minecraft:textures/models/armor/iron_layer_1.png")
                             end
                             for _, modelPart in ipairs({models.models.ex_skill_4.Zombie.ZUpperBody.ZBody.ZLeggingsB,models.models.ex_skill_4.Zombie.ZLowerBody.ZLegs.ZRightLeg.ZLeggingsRL, models.models.ex_skill_4.Zombie.ZLowerBody.ZLegs.ZLeftLeg.ZLeggingsLL}) do
-                                modelPart:setPrimaryTexture("RESOURCE", "minecraft:textures/models/armor/iron_layer_2.png")
+                                modelPart:setPrimaryTexture("RESOURCE", gameVersion >="1.21.2" and "minecraft:textures/entity/equipment/humanoid_leggings/iron.png" or "minecraft:textures/models/armor/iron_layer_2.png")
                             end
                             models.models.ex_skill_4.Creeper:setPrimaryTexture("RESOURCE", "minecraft:textures/entity/creeper/creeper.png")
                             self.exSkill[4].init = true
