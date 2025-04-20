191e9de8d4eab9b528ea6500224745d67950714a
Gakuto1112 <gakuto131112@gmail.com>
2025-04-20 18:49:27 +0900

    使用しない設置物を削除

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -381,15 +381,6 @@ BlueArchiveCharacter = {
         }
 
         instance.placementObjects = {
-            {
-                model = models.models.placement_object.PlacementObject;
-
-                boundingBox = {
-                    size = vectors.vec3(8, 8, 8)
-                };
-
-                placementMode = "COPY";
-            };
         }
 
         instance.exSkill = {
