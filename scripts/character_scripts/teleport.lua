b6926590dab29768de808e54aee1f923a141790d
Gakuto1112 <gakuto131112@gmail.com>
2025-06-15 17:17:28 +0900

    ようなパラメータを削除

--- scripts/character_scripts/teleport.lua
+++ scripts/character_scripts/teleport.lua
@@ -1,7 +1,6 @@
 ---@class Teleport : AvatarModule 忍術ワープを表現するクラス
 ---@field package playerPosPrev Vector3 前ティックのプレイヤーの位置
 ---@field package isStandingPrev boolean 前ティックにプレイヤーが立っていたかどうか
----@field package bodyYawPrev number 前ティックの体の向き
 ---@field package healthPrev integer 前ティックのHP量
 
 Teleport = {
@@ -14,7 +13,6 @@ Teleport = {
 
         instance.playerPosPrev = player:getPos()
         instance.isStandingPrev = player:getPose() == "STANDING" and player:getVehicle() == nil
-        instance.bodyYawPrev = player:getBodyYaw()
         instance.healthPrev = player:getHealth()
 
         return instance
@@ -29,12 +27,11 @@ Teleport = {
             local playerPos = player:getPos()
             local isStanding = player:getPose() == "STANDING" and player:getVehicle() == nil
             if playerPos:copy():sub(self.playerPosPrev):length() - 1 > player:getVelocity():length() and isStanding and self.isStandingPrev and self.healthPrev > 0 then
-                pings.teleport(playerPos, self.playerPosPrev, self.bodyYawPrev)
+                pings.teleport(playerPos, self.playerPosPrev)
             end
 
             self.playerPosPrev = playerPos
             self.isStandingPrev = isStanding
-            self.bodyYawPrev = player:getBodyYaw()
             self.healthPrev = player:getHealth()
         end)
     end;
@@ -43,8 +40,7 @@ Teleport = {
 ---テレポート時の演出（パーティクル、狐の人形）
 ---@param currentPos Vector3 テレポート先のワールド座標
 ---@param previousPos Vector3 テレポート元のワークフロー座標
----@param previousRot number テレポート時の体の向き
-function pings.teleport(currentPos, previousPos, previousRot)
+function pings.teleport(currentPos, previousPos)
     for _ = 1, 70 do
         particles:newParticle(AvatarInstance.compatibilityUtils:checkParticle("minecraft:poof"), currentPos:copy():add(math.random() * 2 - 1, math.random() * 3 - 0.5, math.random() * 2 - 1))
         particles:newParticle(AvatarInstance.compatibilityUtils:checkParticle("minecraft:poof"), previousPos:copy():add(math.random() * 2 - 1, math.random() * 3 - 0.5, math.random() * 2 - 1))
