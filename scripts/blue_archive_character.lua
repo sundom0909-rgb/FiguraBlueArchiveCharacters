e28175a855c4d341d74cd7abccadef690d90ff71
Gakuto1112 <gakuto131112@gmail.com>
2025-10-24 19:02:31 +0900

    水着衣装の髪の物理演算処理のパラメーターを変更

--- scripts/blue_archive_character.lua
+++ scripts/blue_archive_character.lua
@@ -817,6 +817,8 @@ BlueArchiveCharacter = {
                         for _, modelPart in ipairs({models.models.main.Avatar.Head.HairTails.RightHairTail.RightHairTailZPivot.RightHairTailAccessories.RightHairTailAccessory2, models.models.main.Avatar.Head.HairTails.RightHairTail.RightHairTailZPivot.RightHairTailAccessories.RightHairTailAccessory3, models.models.main.Avatar.Head.HairTails.LeftHairTail.LeftHairTailZPivot.LeftHairTailAccessories.LeftHairTailAccessory2, models.models.main.Avatar.Head.HairTails.LeftHairTail.LeftHairTailZPivot.LeftHairTailAccessories.LeftHairTailAccessory3, models.models.main.Avatar.UpperBody.Body.Backpack, models.models.main.Avatar.UpperBody.Body.Skirt, models.models.main.Avatar.UpperBody.Arms.RightArm.RightTrinityLogo, models.models.main.Avatar.UpperBody.Arms.LeftArm.LeftTrinityLogo}) do
                             modelPart:setVisible(false)
                         end
+                        self.physics.physicData[1].x.vertical.max = 0
+                        self.physics.physicData[1].x.vertical.headX.max = 0
                         self.physics.physicData[2].z.vertical.neutral = 5
                         self.physics.physicData[2].z.vertical.headX.max = 5
                         self.physics.physicData[3].z.vertical.neutral = -5
@@ -1031,6 +1033,8 @@ BlueArchiveCharacter = {
                     for _, modelPart in ipairs({models.models.main.Avatar.Head.HairTails.RightHairTail.RightHairTailZPivot.RightHairTailAccessories.RightHairTailAccessory2, models.models.main.Avatar.Head.HairTails.RightHairTail.RightHairTailZPivot.RightHairTailAccessories.RightHairTailAccessory3, models.models.main.Avatar.Head.HairTails.LeftHairTail.LeftHairTailZPivot.LeftHairTailAccessories.LeftHairTailAccessory2, models.models.main.Avatar.Head.HairTails.LeftHairTail.LeftHairTailZPivot.LeftHairTailAccessories.LeftHairTailAccessory3, models.models.main.Avatar.UpperBody.Body.Backpack, models.models.main.Avatar.UpperBody.Body.Skirt, models.models.main.Avatar.UpperBody.Arms.RightArm.RightTrinityLogo, models.models.main.Avatar.UpperBody.Arms.LeftArm.LeftTrinityLogo}) do
                         modelPart:setVisible(true)
                     end
+                    self.physics.physicData[1].x.vertical.max = 90
+                    self.physics.physicData[1].x.vertical.headX.max = 90
                     self.physics.physicData[2].z.vertical.neutral = 20
                     self.physics.physicData[2].z.vertical.headX.max = 20
                     self.physics.physicData[3].z.vertical.neutral = -20
